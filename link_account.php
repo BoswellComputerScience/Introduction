11730a259637f0f16afb0a251b326e304c773299
Lane Surface <surfacelane@gmail.com>
2015-09-04 14:31:02 -0500

    Create link_account.php

---
+++ link_account.php
@@ -0,0 +1,7 @@
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+// TODO: API link
+$api_response = http_get("api.github.com/boswellcomputerscience",, $api_info);
+print_r($info);

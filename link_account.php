e29650cd7eb9f8fad2918c92b800bca751bb4934
Lane Surface <surfacelane@gmail.com>
2015-11-14 21:05:22 -0600

    Minor UI Changes
    Implemented some minor UI changes, added capability to add users to the
    GitHub organization, and finally updated links

--- link_account.php
+++ link_account.php
@@ -1,10 +1,26 @@
 <?php
 
 	$username = $_POST["username"];
-	$password = $_POST["password"];
 
-	// TODO: API link
-	$api_response = http_get("api.github.com/orgs/boswellcomputerscience",, $api_info);
-	print_r($info);
+	$url = 'api.github.com/orgs/boswellcomputerscience/memberships/' . $username;
+	$fields = array(
+		'role' => urlencode('member')
+	);
 
+	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+	rtrim($fields_string, '&');
+	
+	$ch = curl_init();
+	
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	$redirect_url = 'https://github.com/boswellcomputerscience';
+	header('Location: ' . $redirect_url);
+	die();
 ?>
